751e5d7b9040c2b38dd3e2f3c6e19f6916f9923e
Jeni Williams <stanfordjeni@gmail.com>
2017-09-13 17:01:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var newObject = Object.assign({}, object, { [key]: value })
   return newObject
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object)
+  newObject.delete([key])
+  return newObject
+}
